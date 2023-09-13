69b0d65469e4500d195ef19a6a3d53dc5e890677
John10Alvarezz <jalvarez2023@alu.uct.cl>
2023-09-12 21:09:31 -0300

    falta

--- Ejercicios Semana 6/Clase de herencia/Ejercicios/Ejercicio 4.py
+++ Ejercicios Semana 6/Clase de herencia/Ejercicios/Ejercicio 4.py
@@ -1,2 +1,5 @@
 class Reserva:
-    def __init__(self):
+    def __init__(self, nombre_del_pasajero, numero_de_vuelo, fecha):
+        self.nombre_del_pasajero = nombre_del_pasajero
+        self.numero_de_vuelo = numero_de_vuelo
+        self.fecha = fecha
